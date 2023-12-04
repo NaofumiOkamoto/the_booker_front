3967516a5443f61f26309daa7367d17dc30f5ec7
NaofumiOkamoto <n.okamooo@gmail.com>
2023-12-04 18:15:15 +0900

    add: expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hellllllloooo</Text>
+    </View>
+  )
+}
+
+export default Index
